496e2d136128af8ccb2f8d16c7124cc7cef46843
unknown <w.andrew.pedersen@gmail.com>
2019-07-30 10:32:41 -0600

    Added Routes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import Products from './components/Products'
+
+
+function App() { 
+  return (
+    <Router>
+      <Route path='/'component={Navigation} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/products' component={Products} />  
+      </Switch>
+    </Router> 
+  );
+}
+
+export default App;
