5a6fb9a7ace8a9ca7adbbbe2310fd373b2519ac8
unknown <w.andrew.pedersen@gmail.com>
2019-07-31 12:56:17 -0600

    Stripe Hype integrated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './components/Home'
 import Navigation from './components/Navigation'
 import ProductsList from './components/ProductsList'
 import ProductDetail from './components/ProductDetail'
+import PaymentSuccess from './components/PaymentSuccess'
+import PaymentCancel from './components/PaymentCancel'
 
 
 function App() { 
@@ -15,7 +17,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/products' component={ProductsList} />
-        <Route path='/products/:id' component={ProductDetail} />    
+        <Route path='/products/:id' component={ProductDetail} />
+        <Route path='/success' component={PaymentSuccess} />   
+        <Route path='/cancel' component={PaymentCancel} />  
       </Switch>
     </Router> 
   );
--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -21,7 +21,16 @@ class ProductDetail extends Component {
     initiateStripeCheckout = async () => {
         console.log('initiating')
         const stripe = window.Stripe('pk_test_MTQSb4lGSImPrW41fw1Fe3EO00FsKG3fxd')
+        const { product } = this.state
 
+        const lineItem = {
+            name: product.name,
+            description: product.description,
+            images: [product.img_Url],
+            amount: product.price,
+            currency: 'usd',
+            quantity: 1,
+          }
         try {
           // Initiate checkout session to get session id
           const response = await fetch('http://localhost:4000/api/checkout', {
@@ -29,7 +38,8 @@ class ProductDetail extends Component {
             headers: {
               'Content-Type': 'application/json',
               'Accept': 'application/json'
-            }
+            },
+            body: JSON.stringify(lineItem)
           })
           const data = await response.json()
           const sessionId = data.session.id
