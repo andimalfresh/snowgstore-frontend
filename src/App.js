429186876199ed836b8ca97ea276155279642460
unknown <w.andrew.pedersen@gmail.com>
2019-08-07 13:33:13 -0600

    auth0 integration setup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ProductsList from './components/ProductsList'
 import ProductDetail from './components/ProductDetail'
 import PaymentSuccess from './components/PaymentSuccess'
 import PaymentCancel from './components/PaymentCancel'
+import Profile from './components/Profile'
 
 
 function App() { 
@@ -19,7 +20,8 @@ function App() {
         <Route exact path='/products' component={ProductsList} />
         <Route path='/products/:id' component={ProductDetail} />
         <Route path='/success' component={PaymentSuccess} />   
-        <Route path='/cancel' component={PaymentCancel} />  
+        <Route path='/cancel' component={PaymentCancel} />
+        <Route path='/profile' component={Profile} />  
       </Switch>
     </Router> 
   );
